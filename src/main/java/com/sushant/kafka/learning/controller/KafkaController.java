f9c05f19a388d43dc513669c7d91110b96f6e2d3
Sushant <sushant.dhiman9812@gmail.com>
2024-10-12 16:21:21 +0530

    Kafka Producer

---
+++ src/main/java/com/sushant/kafka/learning/controller/KafkaController.java
@@ -0,0 +1,25 @@
+package com.sushant.kafka.learning.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sushant.kafka.learning.services.KafkaProducerService;
+
+@RestController
+@RequestMapping(value = "/api/v1/kafka")
+public class KafkaController {
+
+    @Autowired
+    private KafkaProducerService kafkaProducerService;
+    
+    @RequestMapping(value = "/produce", method = RequestMethod.POST)
+    public void produceData(final @RequestBody Object data,
+                            final @RequestParam(name = "topic") String topic,
+                            final @RequestParam(name = "key") String key) {
+        kafkaProducerService.sendMessage(topic, key, data.toString());
+    }
+}
